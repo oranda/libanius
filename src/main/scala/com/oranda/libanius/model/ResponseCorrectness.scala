1ed4c3b78f607a34b5f7ed9dde51bd455b63111a
James McCabe <jjtmccabe@gmail.com>
2020-08-25 04:08:51 +0200

    Add ResponseCorrectness file

---
+++ src/main/scala/com/oranda/libanius/model/ResponseCorrectness.scala
@@ -0,0 +1,25 @@
+/*
+ * Libanius
+ * Copyright (C) 2012-2020 James McCabe <jjtmccabe@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.oranda.libanius.model
+
+sealed abstract class ResponseCorrectness
+
+case object Correct extends ResponseCorrectness
+case object Incorrect extends ResponseCorrectness
+case object ItemNotFound extends ResponseCorrectness
