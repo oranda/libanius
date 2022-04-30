03d7d6d4228889c07110d4134aefc1886ff22183
James McCabe <jjtmccabe@gmail.com>
2022-04-30 11:38:45 -0100

    Update copyright notice to 2022

--- src/test/scala/com/oranda/libanius/model/action/serialize/CustomFormatParserFastSpec.scala
+++ src/test/scala/com/oranda/libanius/model/action/serialize/CustomFormatParserFastSpec.scala
@@ -1,6 +1,6 @@
 /*
  * Libanius
- * Copyright (C) 2012-2020 James McCabe <jjtmccabe@gmail.com>
+ * Copyright (C) 2012-2022 James McCabe <jjtmccabe@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -18,7 +18,7 @@
 
 /*
  * Libanius
- * Copyright (C) 2012-2020 James McCabe <jjtmccabe@gmail.com>
+ * Copyright (C) 2012-2022 James McCabe <jjtmccabe@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
